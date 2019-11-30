e5450bff24547f2e2c81ef89de128cda85091dda
Ashikin Talha <39068958+Ashikwome@users.noreply.github.com>
2019-11-30 19:15:09 +0600

    First Running Django Project

---
+++ WebDesign/wsgi.py
@@ -0,0 +1,17 @@
+# it is webserver getway interfaces
+"""
+WSGI config for WebDesign project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebDesign.settings')
+
+application = get_wsgi_application()
